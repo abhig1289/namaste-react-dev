990b5ed834b8968e1d65225d834cd4ca147543d2
abhilash gujjeti <gujjetiabhilash1289@gmail.com>
2023-01-20 14:25:29 +0530

    lecture 2

--- App.js
+++ App.js
@@ -1,3 +1,27 @@
+    import { minify } from "csso";
+import React from "react";
+    import ReactDOM  from "react-dom/client";
+    
+// HMR- Hot Module Replacement
+// Created a Server 
+// File watch algorithm -C++
+// Bundling
+// minify
+// cleaning our coding
+// Dev and Production build
+// Super fast build algorithm
+// Image Optimization
+//Caching by development
+// Compression
+// Compatible with older versions of Chrome
+// HTTPS on dev
+//  Port number
+// Consistent Hashing algoritm
+// Zero config
+// 
+// 
+// Transitive dependencies
+
     const heading = React.createElement("h1",{
         id:'title',
         className:'title',
@@ -9,7 +33,7 @@
     const heading2 = React.createElement("h1",{
         id:'title2',
         className:'title2',
-    },"Namaste Abhilash 2");
+    },"Namaste Abhilash g 2");
     const container =React.createElement("div",{id:"container"},[heading1,heading2]);
     const root = ReactDOM.createRoot(document.getElementById("root"));
     // passing the react element inside the root
---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,24 @@
+const heading = React.createElement("h1", {
+    id: "title",
+    className: "title"
+}, "Namaste Abhilash");
+const heading1 = React.createElement("h1", {
+    id: "title1",
+    className: "title1"
+}, "Namaste Abhilash 1");
+const heading2 = React.createElement("h1", {
+    id: "title2",
+    className: "title2"
+}, "Namaste Abhilash 2");
+const container = React.createElement("div", {
+    id: "container"
+}, [
+    heading1,
+    heading2
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// passing the react element inside the root
+// root.render(heading)
+root.render(container);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
