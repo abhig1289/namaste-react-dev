a4786066ae15bfa7a626719e6b25563c49c30e6c
abhilash gujjeti <gujjetiabhilash1289@gmail.com>
2023-02-13 23:31:54 +0530

    chapter 6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,47 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { RestauranList } from "../constants";
 import RestaurantCard from "./RestaurantCard";
-
+import Shimmer from "./Shimmer";
 
 const Body = () => {
   // what are react hooks , state ,useState
   //search texxt is a local state variable
   const [searchInput, setSearchInput] = useState(""); // to create state variables returns=[variable,setfunction to update the state variable]
   const [searchClicked, setSearchClicked] = useState("false");
-  const [restaurants, setRestaurants] = useState(RestauranList);
-  function filteData(searchInput,restaurants){
-    const filterData=restaurants.filter((rest) =>rest.data.name.includes(searchInput))
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [allRestaurants, setAllRestaurants] = useState([]);
+  function filteData(searchInput, restaurants) {
+    const filterData = restaurants.filter((rest) =>
+      rest?.data?.name?.toLowerCase()?.includes(searchInput?.toLowerCase())
+    );
     return filterData;
-  };
-  return (
+  }
+  // empty dependency array => once after render
+  // dep arry [searchText] => once after initial render + everytime after redern (my searchText changes)
+
+  async function getRestaurants() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4306076&lng=78.3813785&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    //Optional Chaining
+    setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+    setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+    console.log(json);
+  }
+  console.log("render");
+  useEffect(() => {
+    // console.log("useEffect")
+    //API Calls
+    getRestaurants();
+  }, []);
+  //not render anything or early return
+  if (!allRestaurants) return null;
+  // if (filteredRestaurants.length===0) return <h1>No restaurant matches the search....!!!</h1>;
+  //Conditional rendering(no data shimmerUI , if Data actual UI)
+  return allRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <>
       <div className="search-container">
         <input
@@ -33,8 +61,8 @@ const Body = () => {
             //     setSearchClicked("false")
             // }}
             //need to filter the data and  update the restaurants accordingly
-            const data = filteData(searchInput, restaurants);
-            setRestaurants(data);
+            const data = filteData(searchInput, allRestaurants);
+            setFilteredRestaurants(data);
           }}
         >
           Search
@@ -45,7 +73,7 @@ const Body = () => {
           // restaurant={RestauranList[0]}
           {...RestauranList[0].data}
           /> */}
-        {restaurants.map((restaurant, index) => {
+        {filteredRestaurants.length===0 ? <> No restaurant matches the search....!!! </> : filteredRestaurants.map((restaurant, index) => {
           return (
             <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
           );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
 
+const LoggedInUser = () => {
+  //API call to authente
+  return true;
+};
 const Title = () => (
-    //   <h1 id="title" key="title key11">
-    //  Daaaa Tinu
-    //   </h1>
-    <a href="/">
-      <img
-        src="http://wirally.com/wp-content/uploads/2019/01/3-daa-tinu.jpg"
-        alt="logo"
-        className="logo"
-      />
-    </a>
-  );
-  const HeaderComponent = () => {
-    return (
-      <div className="header">
-        {/* {jsxHeading}
+  //   <h1 id="title" key="title key11">
+  //  Daaaa Tinu
+  //   </h1>
+  <a href="/">
+    <img
+      src="http://wirally.com/wp-content/uploads/2019/01/3-daa-tinu.jpg"
+      alt="logo"
+      className="logo"
+    />
+  </a>
+);
+const HeaderComponent = () => {
+  const [titleName, setTitleName] = useState("Daa Tinu");
+  const [isLoggedIn,setIsLoggedIn]=useState(false)
+  // console.log("render")
+  return (
+    <div className="header">
+      {/* {jsxHeading}
           {Heading()}
           <Heading/> 
           {10+1}<br/>
           {string}
           {console.log(string)}
         <h1>Namaste React Functional Component</h1> */}
-        <Title />
-        <div className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Cart</li>
-          </ul>
-        </div>
+      <Title />
+      {/* <button onClick={()=>setTitleName("new food app")}>click button</button>
+        <h1>{titleName}</h1> */}
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
       </div>
-    );
-  };
-export default HeaderComponent
+      {isLoggedIn ? <button onClick={()=>setIsLoggedIn(false)}>login</button> : <button onClick={()=>setIsLoggedIn(true)}>logout</button>}
+    </div>
+  );
+};
+export default HeaderComponent;
+// javascript expression vs statements
